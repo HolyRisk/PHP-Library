338efe0f4ad102cfaf2be97f5c77ad992863d7c7
Holyrisk <792034260@qq.com>
2020-12-30 12:01:25 +0800

    加入 composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '99a3afce1aabe2ff36bc98f043e9612aff081b1d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '99a3afce1aabe2ff36bc98f043e9612aff081b1d',
+    ),
+  ),
+);
